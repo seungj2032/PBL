ecfaaae64d492fbb584b18e6e34526cd1b03eb63
seungj2032 <122359772+seungj2032@users.noreply.github.com>
2023-11-14 17:31:35 +0900

    Add files via upload

---
+++ pbl7.js
@@ -0,0 +1,12 @@
+let red = document.getElementById('red');
+let orange = document.getElementById('orange');
+let yellow = document.getElementById('yellow');
+let green = document.getElementById('green');
+let blue = document.getElementById('blue');
+let navy = document.getElementById('navy');
+let purple = document.getElementById('purple');
+let pink = document.getElementById('pink');
+
+let c1 = 0; let c2 = 0; let c3 = 0; let c4 = 0;
+let c5 = 0; let c6 = 0; let c7 = 0; let c8 = 0;
+
