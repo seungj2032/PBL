7c0dac25a5af912d80b59b30e4781f77819e0f5e
seungj2032 <122359772+seungj2032@users.noreply.github.com>
2023-11-14 19:52:14 +0900

    Update pbl7.js

--- pbl7.js
+++ pbl7.js
@@ -7,6 +7,56 @@ let navy = document.getElementById('navy');
 let purple = document.getElementById('purple');
 let pink = document.getElementById('pink');
 
-let c1 = 0; let c2 = 0; let c3 = 0; let c4 = 0;
-let c5 = 0; let c6 = 0; let c7 = 0; let c8 = 0;
+let disp1 = document.querySelector('.c1');
+let disp2 = document.querySelector('.c2');
+/*let colorArray = document.querySelectorAll('.colors');
 
+console.log(colorArray);*/
+
+red.addEventListener('click', function() {  
+    color = "red";
+    selected()
+})
+orange.addEventListener('click', function() {  
+    color = "orange";
+    selected()
+})
+yellow.addEventListener('click', function() {  
+    color = "yellow";
+    selected()
+})
+green.addEventListener('click', function() {  
+    color = "green";
+    selected()
+})
+blue.addEventListener('click', function() {  
+    color = "blue";
+    selected()
+})
+navy.addEventListener('click', function() {  
+    color = "navy";
+    selected()
+})
+purple.addEventListener('click', function() {  
+    color = "purple";
+    selected()
+})
+pink.addEventListener('click', function() {  
+    color = "pink";
+    selected()
+})
+
+function selected() {
+    /*disp1.style.display = 'none';
+    disp2.style.display = 'flex';
+
+    switch (color) {
+        case red:
+            
+            break;
+    
+        default:
+            break;
+    }*/
+    console.log(color);
+}
